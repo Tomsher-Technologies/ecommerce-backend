2ba5f14c90876add9437cafaba62d0c3228f36ed
akmaltomsher <akmal@tomsher.com>
2024-05-09 13:38:43 +0400

    all

---
+++ config/allowed-origins.ts
@@ -0,0 +1,5 @@
+export const allowedOrigins: string[] = [
+    'https://timehouse-client-akmaltomsher-tomshers-projects.vercel.app',
+    'https://timehouse-client-phlezjgm1-tomshers-projects.vercel.app/',
+    'http://localhost:3027'
+  ];
