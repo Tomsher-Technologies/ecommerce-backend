49ee398739f24bcd3ef13371e12a904f161d5bc5
akmaltomsher <akmal@tomsher.com>
2024-05-25 09:53:09 +0400

    all

--- routes/admin/stores/warehouse-routes.ts
+++ routes/admin/stores/warehouse-routes.ts
@@ -7,19 +7,19 @@ import authMiddleware from '../../../middleware/admin/auth-middleware';
 import userPermissionMiddleware from '../../../middleware/admin/admin-user-permission-roll-middleware';
 import { permissionBlocks } from '../../../src/constants/permission-blocks';
 
-import LanguagesController from '../../../src/controllers/admin/setup/languages-controller';
+import WarehouseController from '../../../src/controllers/admin/stores/warehouse-controller';
 
 const router: Router = express.Router();
 
 
 router.use(authMiddleware);
 
-router.get('/', logResponseStatus, userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, readOnly: 1 }), LanguagesController.findAll);
-router.get('/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, readOnly: 1 }), LanguagesController.findOne);
-router.post('/', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), logResponseStatus, LanguagesController.create);
-router.post('/:id', logResponseStatus, userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), LanguagesController.update);
-router.post('/status-change/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), LanguagesController.statusChange);
-router.delete('/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse }), LanguagesController.destroy);
+router.get('/', logResponseStatus, userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, readOnly: 1 }), WarehouseController.findAll);
+router.get('/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, readOnly: 1 }), WarehouseController.findOne);
+router.post('/', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), logResponseStatus, WarehouseController.create);
+router.post('/:id', logResponseStatus, userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), WarehouseController.update);
+router.post('/status-change/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse, writeOnly: 1 }), WarehouseController.statusChange);
+router.delete('/:id', userPermissionMiddleware({ permissionBlock: permissionBlocks.stores.warehouse }), WarehouseController.destroy);
 
 
 
