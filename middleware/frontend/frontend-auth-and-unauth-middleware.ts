e544f3e3c448fe76df2be673182f3f63c3e01e09
HannaTomsher <hanna.n@tomsher.com>
2024-06-19 18:13:40 +0400

    error fixing

---
+++ middleware/frontend/frontend-auth-and-unauth-middleware.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
+
+import CustomerModel from '../../src/model/frontend/customers-model';
+
+interface CustomRequest extends Request {
+    user?: any;
+}
+const frontendAuthAndUnAuthMiddleware = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    try {
+        const authHeader = req.header('Authorization');
+        const uuid = req.headers['uuid']
+
+        if (!authHeader && !uuid) {
+            return res.status(401).json({ message: 'Unauthorized - Missing Authorization header', status: false });
+        }
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+            if (!token) {
+                return res.status(401).json({ message: 'Unauthorized - Missing token', status: false });
+            }
+            const checkToken: any = jwt.verify(token, `${process.env.TOKEN_SECRET_KEY}`);
+
+
+            if (checkToken?.userId) {
+                const userData = await CustomerModel.findOne({ _id: checkToken.userId });
+                if (userData) {
+                    res.locals.user = userData._id;
+                    next();
+                } else {
+                    return res.status(404).json({ message: 'User data not found!', status: false });
+                }
+            }
+        } else if (uuid) {
+            res.locals.user = uuid;
+            next();
+        }
+
+        else {
+            return res.status(401).json({ message: 'Unauthorized - Invalid token', status: false, reLogin: true });
+        }
+    } catch (error) {
+        console.error(error);
+        if (error instanceof jwt.JsonWebTokenError) {
+            return res.status(400).json({ message: 'Invalid token', status: false });
+        }
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+export default frontendAuthAndUnAuthMiddleware;
