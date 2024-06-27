b92d9d7476c3c12692ee728b776914a0a4a4be46
HannaTomsher <hanna.n@tomsher.com>
2024-06-27 13:56:38 +0400

    cart model

--- src/constants/offers.ts
+++ src/constants/offers.ts
@@ -3,3 +3,9 @@ export const offers = {
     category: 'category',
     product: 'product'
 }
+
+export const offerTypes = {
+    percent: 'percent',
+    amountOff: 'amount-off',
+    buyXgetY: 'buy-x-get-y',
+};
--- src/utils/schemas/frontend/guest/cart-schema.ts
+++ src/utils/schemas/frontend/guest/cart-schema.ts
@@ -2,10 +2,14 @@ import { z as zod } from 'zod';
 
 export const cartSchema = zod.object({
     customerId: zod.string().optional(),
-
+    slug: zod.string().optional(),
+    variantId: zod.string().optional(),
+    quantity: zod.number(),
     guestUserId: zod.any().optional(),
 
-}).nonstrict();
+}).refine(data => (data.slug !== undefined || data.variantId !== undefined), {
+    message: 'Either "slug" or "variantId" must be provided',
+});
 export const cartProductSchema = zod.object({
     customerId: zod.string().optional(),
     variantId: zod.string().optional(),
