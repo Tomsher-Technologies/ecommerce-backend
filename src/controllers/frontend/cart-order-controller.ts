8fcecd6a957c701fc20908bb363833be974d232d
HannaTomsher <hanna.n@tomsher.com>
2024-06-30 21:50:15 +0400

    cart changes

--- src/controllers/frontend/cart-order-controller.ts
+++ src/controllers/frontend/cart-order-controller.ts
@@ -286,12 +286,14 @@ class CartController extends BaseController {
                                         if (giftWrapAmount && giftWrapAmount.blockValues && giftWrapAmount.blockValues.enableGiftWrap && giftWrapAmount.blockValues.enableGiftWrap == true) {
                                             giftWrapCharge = Number(giftWrapAmount.blockValues.giftWrapCharge)
                                         }
-                                        const removeGiftWrapAmount: 0 = existingCartProduct.totalGiftWrapAmount > 0 && giftWrapCharge ? existingCart.totalGiftWrapAmount - giftWrapCharge : existingCart.totalGiftWrapAmount
+                                        const removeGiftWrapAmount: 0 = (existingCartProduct.giftWrapAmount > 0 && giftWrapCharge) ? (existingCart.totalGiftWrapAmount - giftWrapCharge) : existingCart.totalGiftWrapAmount
+                                        console.log("++++++++++++++++++++++++", removeGiftWrapAmount);
+
                                         const cartUpdate = await CartService.update(existingCartProduct.cartId, {
                                             totalProductAmount: totalAmountOfProduct,
                                             totalDiscountAmount: totalDiscountAmountOfProduct,
                                             totalAmount: (totalAmountOfProduct - removeGiftWrapAmount) + shippingCharge,
-                                            totalGiftWrapAmount: existingCartProduct.totalGiftWrapAmount > 0 ? existingCart.totalGiftWrapAmount - existingCartProduct.totalGiftWrapAmount : existingCart.totalGiftWrapAmount
+                                            totalGiftWrapAmount: existingCartProduct.giftWrapAmount > 0 ? (existingCart.totalGiftWrapAmount - removeGiftWrapAmount) : existingCart.totalGiftWrapAmount
                                         });
 
                                         const checkCartProducts = await CartService.findAllCart({ cartId: existingCartProduct.cartId })
