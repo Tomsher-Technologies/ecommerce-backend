c27e68ff8ffbebc5e7e52920eee5f7fe3776b584
akmaltomsher <akmal@tomsher.com>
2024-07-02 23:03:47 +0400

    all

--- app.ts
+++ app.ts
@@ -25,6 +25,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 200,
 }));
