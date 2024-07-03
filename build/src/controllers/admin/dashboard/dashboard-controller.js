b85e1ca790530739120467daf26c74a450eba57a
akmaltomsher <akmal@tomsher.com>
2024-07-03 08:36:54 +0400

    all

--- build/src/controllers/admin/dashboard/dashboard-controller.js
+++ build/src/controllers/admin/dashboard/dashboard-controller.js
@@ -11,26 +11,45 @@ const controller = new base_controller_1.default();
 class DashboardController extends base_controller_1.default {
     async dashboard(req, res) {
         try {
-            const { page_size = 1, limit = 10, status = ['0', '1', '2'], sortby = '', sortorder = '', keyword = '', fromDate = '', endDate = '' } = req.query;
+            const { page_size = 1, limit = 10, status = ['0', '1', '2'], sortby = '', sortorder = '', keyword = '', fromDate, endDate } = req.query;
             let query = { _id: { $exists: true } };
             const userData = await res.locals.user;
             const countryId = (0, helpers_1.getCountryId)(userData);
             if (countryId) {
                 query.countryId = countryId;
             }
-            if (status && status !== '') {
-                query.status = { $in: Array.isArray(status) ? status : [status] };
+            // if (status && status !== '') {
+            //     query.status = { $in: Array.isArray(status) ? status : [status] };
+            // } else {
+            //     query.cartStatus != '1';
+            // }
+            query = { cartStatus: { $ne: "1" } };
+            const today = new Date();
+            today.setHours(0, 0, 0, 0);
+            let salesQuery = {};
+            // if (fromDate || endDate) {
+            //     salesQuery.createdAt = {
+            //         ...(fromDate && { $gte: new Date(fromDate) }),
+            //         ...(endDate && { $lte: dateConvertPm(endDate) })
+            //     };
+            // }
+            let currentDate = new Date();
+            currentDate.setHours(0, 59, 59, 59);
+            // Subtract 7 days from the current date
+            today.setDate(today.getDate() - 7);
+            if (!fromDate || !endDate) {
+                salesQuery.createdAt = {
+                    ...(today && { $gte: today }),
+                    ...(today && { $lte: currentDate })
+                };
             }
             else {
-                query.status = '1';
-            }
-            let salesQuery = {};
-            if (fromDate || endDate) {
                 salesQuery.createdAt = {
                     ...(fromDate && { $gte: new Date(fromDate) }),
                     ...(endDate && { $lte: (0, helpers_1.dateConvertPm)(endDate) })
                 };
             }
+            // salesQuery.cartStatus= { $ne: "1" } }
             const sort = {};
             if (sortby && sortorder) {
                 sort[sortby] = sortorder === 'desc' ? -1 : 1;
@@ -47,21 +66,20 @@ class DashboardController extends base_controller_1.default {
                 query,
                 sort
             });
-            console.log("...........", query);
             const totalSales = await dashboard_service_1.default.findTotalSales({
                 salesQuery,
-                fromDate,
-                endDate
+                fromDate: fromDate ? fromDate : today,
+                endDate: endDate ? endDate : currentDate
             });
             const totalOrders = await dashboard_service_1.default.findTotalOrder({
                 salesQuery,
-                fromDate,
-                endDate
+                fromDate: fromDate ? fromDate : today,
+                endDate: endDate ? endDate : currentDate
             });
             controller.sendSuccessResponse(res, {
                 requestedData: {
                     overViewCount: dashboard,
-                    salesComparison: salesComparison,
+                    salesAndOrderComparison: salesComparison,
                     totalSales: totalSales,
                     totalOrders: totalOrders
                 },
--- build/src/controllers/admin/general/general-controller.js
+++ build/src/controllers/admin/general/general-controller.js
@@ -3,20 +3,34 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const base_controller_1 = __importDefault(require("../base-controller"));
+const base_controller_1 = __importDefault(require("../../../controllers/admin/base-controller"));
+const country_model_1 = __importDefault(require("../../../model/admin/setup/country-model"));
+const website_setup_model_1 = __importDefault(require("../../../model/admin/setup/website-setup-model"));
+const website_setup_1 = require("../../../constants/website-setup");
 const controller = new base_controller_1.default();
 class GeneralController extends base_controller_1.default {
-    async findPage(req, res) {
-        try {
-            // return controller.sendSuccessResponse(res, {
-            //     page: page.page,
-            //     pageReference: page.pageReference,
-            //     linkType: page.linkType,
-            // }, 200);
+    async getGeneralSettings(req, res) {
+        const countryDetails = await country_model_1.default.findOne({ isOrigin: true });
+        if (!countryDetails) {
+            return controller.sendErrorResponse(res, 200, {
+                message: 'Country not set yet!',
+            });
         }
-        catch (error) {
-            // return controller.sendErrorResponse(res, 500, { message: error.message || 'Some error occurred while fetching banners' });
+        const websiteDetails = await website_setup_model_1.default.findOne({ countryId: countryDetails._id, blockReference: website_setup_1.blockReferences.websiteSettings });
+        if (!websiteDetails) {
+            return controller.sendErrorResponse(res, 200, {
+                message: 'Website details not found!',
+            });
         }
+        if (websiteDetails && websiteDetails?.blockValues && websiteDetails?.blockValues?.websiteLogoUrl) {
+            return controller.sendSuccessResponse(res, {
+                requestedData: websiteDetails?.blockValues,
+                message: 'Website settings not found!'
+            }, 200);
+        }
+        return controller.sendErrorResponse(res, 200, {
+            message: 'Website settings not found!',
+        });
     }
 }
-exports.default = GeneralController;
+exports.default = new GeneralController();
