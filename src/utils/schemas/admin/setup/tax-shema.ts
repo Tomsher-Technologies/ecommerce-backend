40fce6639e8c82629a7752bb24c979e931ab17d6
HannaTomsher <hanna.n@tomsher.com>
2024-07-15 14:12:14 +0400

    tax update

--- src/utils/schemas/admin/setup/tax-shema.ts
+++ src/utils/schemas/admin/setup/tax-shema.ts
@@ -2,8 +2,8 @@ import { z as zod } from 'zod';
 
 export const taxSchema = zod.object({
     taxTitle: zod.string({ required_error: 'Tax title is required', }).min(3, 'Tax title is should be 3 chars minimum'),
-    taxPercentage: zod.string({ required_error: 'Tax percentage is required', }).min(2, 'Tax percentage is should be 2 chars minimum'),
-}).nonstrict(); 
+    taxPercentage: zod.string({ required_error: 'Tax percentage is required', }).min(1, 'Tax percentage is should be 1 chars minimum'),
+}).nonstrict();
 
 
 export const taxStatusSchema = zod.object({
