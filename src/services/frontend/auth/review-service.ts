4f725b7cd693f9f4a6a0520ac1ec45d7ca9969a5
HannaTomsher <hanna.n@tomsher.com>
2024-08-22 13:26:11 +0400

    review

--- src/services/frontend/auth/review-service.ts
+++ src/services/frontend/auth/review-service.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import ReviewModel, { ReviewProps } from '../../../model/frontend/review-model';
 
 class ReviewService {
@@ -13,9 +14,83 @@ class ReviewService {
         return ReviewModel.findOneAndUpdate(filter, updateData, { new: true });
     }
 
-    async listReviews(status: string, productId: string): Promise<ReviewProps[]> {
-        return ReviewModel.find({ reviewStatus: status, productId: productId });
+    async listReviews(status: string, productId: string): Promise<any> {
+        const pipeline = [
+            {
+                $match: {
+                    reviewStatus: status,
+                    productId: new mongoose.Types.ObjectId(productId),
+                },
+            },
+            {
+                $group: {
+                    _id: "$rating",
+                    count: { $sum: 1 },
+                },
+            },
+            {
+                $group: {
+                    _id: null,
+                    totalReviews: { $sum: "$count" }, // Calculate total number of reviews
+                    ratingCounts: {
+                        $push: {
+                            rating: "$_id",
+                            count: "$count",
+                        },
+                    },
+                    totalRatingSum: { $sum: { $multiply: ["$_id", "$count"] } }, // Calculate the sum of all ratings
+                },
+            },
+            {
+                $addFields: {
+                    averageRating: {
+                        $divide: ["$totalRatingSum", "$totalReviews"], // Calculate the average rating
+                    },
+                    ratingCounts: {
+                        $map: {
+                            input: "$ratingCounts",
+                            as: "ratingCount",
+                            in: {
+                                rating: "$$ratingCount.rating",
+                                count: "$$ratingCount.count",
+                                percentage: {
+                                    $multiply: [
+                                        { $divide: ["$$ratingCount.count", "$totalReviews"] },
+                                        100,
+                                    ],
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            {
+                $project: {
+                    _id: 0,
+                    averageRating: 1,
+                    totalReviews: 1,
+                    ratingCounts: 1,
+                },
+            },
+        ];
+
+        const reviewStatistics = await ReviewModel.aggregate(pipeline);
+        const reviews = await ReviewModel.find({
+            reviewStatus: status,
+            productId: new mongoose.Types.ObjectId(productId),
+        });
+
+        return {
+            reviews,
+            statistics: reviewStatistics[0] || {
+                averageRating: 0,
+                totalReviews: 0,
+                ratingCounts: [],
+            },
+        };
     }
+
+
 }
 
 export default new ReviewService();
