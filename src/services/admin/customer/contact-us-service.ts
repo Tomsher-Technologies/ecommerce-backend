1e81e1971932c74f547ff3a601cf8bd2d35434ea
HannaTomsher <hanna.n@tomsher.com>
2024-08-22 13:49:14 +0400

    country populate-contactus

--- src/services/admin/customer/contact-us-service.ts
+++ src/services/admin/customer/contact-us-service.ts
@@ -22,9 +22,31 @@ class ContactUsService {
             { $match: query },
             countriesLookup,
             customerLookup,
+            { $unwind: { path: "$country", preserveNullAndEmptyArrays: true } },
+            { $unwind: { path: "$customer", preserveNullAndEmptyArrays: true } },
             { $skip: skip },
             { $limit: limit },
             { $sort: finalSort },
+            {
+                $project: {
+                    _id: 1,
+                    name: 1,
+                    email: 1,
+                    phone: 1,
+                    subject: 1,
+                    message: 1,
+                    status: 1,
+                    createdAt: 1,
+                    updatedAt: 1,
+                    __v: 1,
+                    "country._id": 1,
+                    "country.countryTitle": 1,
+                    "country.slug": 1,
+                    "country.countryCode": 1,
+                    "country.currencyCode": 1,
+                    "customer": 1,
+                }
+            }
         ];
 
         return ContactUsModel.aggregate(pipeline).exec();
