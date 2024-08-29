d156f870d97d3a8ad34f68d50516720902e4c13a
HannaTomsher <hanna.n@tomsher.com>
2024-08-29 13:34:32 +0400

    message

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -15,10 +15,18 @@ export const guestRegisterOtp = (shopName: string, otp: number) => {
     return `Welcome to ${shopName}! Your OTP for guest registration is ${otp}. Please enter this code to complete your guest chekcout.`
 }
 
-export const resendOtp = ( otp: number) => {
+export const resendOtp = (otp: number) => {
     return `Your OTP is ${otp}. Please use this code to complete your verification.`
 }
 
-export const resetPasswordOtp = ( otp: number) => {
+export const resetPasswordOtp = (otp: number) => {
     return `Your OTP for password reset is: ${otp}. Please use this code to proceed with resetting your password.`
+}
+
+export const createOrder = (orderId: string) => {
+    return `Your order has been confirmed. For your reference, your order number is ${orderId}. We shall update you when your order is ready to be shipped.`
+}
+
+export const shippingOrder = (orderId: string, expectedDeliveryDate: string) => {
+    return `Your order #${orderId} has just been dispatched! You can expect it to be delivered by (${expectedDeliveryDate}).`
 }
